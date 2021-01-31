da880df5fc6e594ba871c0bc8725367c9c0e3434
iamhabbeboy <iamhabbeboy@gmail.com>
2021-01-31 09:34:48 +0100

    email migrations

---
+++ database/migrations/2021_01_30_231226_create_attachment_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mail_id');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachment');
+    }
+}
